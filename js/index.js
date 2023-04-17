711e7477fd776c0087de9278e2d194115c8c7f4b
Nikhil26062000 <nikhilsbn06@gmail.com>
2023-04-17 13:35:11 +0530

    Javascript Initiated and added Game Area

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const init = () => {
+    document.getElementById('game-Area').style.display = 'none';
+}
+
+const easyButton = () => {
+    document.getElementById('intro-view').style.display="none";
+    document.getElementById('game-Area').style.display="grid";
+}
