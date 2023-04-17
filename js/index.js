ac70b8cfe35231e85f8a61f8d03e11f1eb11ddd5
Nikhil26062000 <nikhilsbn06@gmail.com>
2023-04-17 15:03:47 +0530

    Game Area modified

--- js/index.js
+++ js/index.js
@@ -4,5 +4,8 @@ const init = () => {
 
 const easyButton = () => {
     document.getElementById('intro-view').style.display="none";
+    document.getElementById('btn-new-game').style.display="none";
+    // document.getElementById('desc').style.display="none";
+
     document.getElementById('game-Area').style.display="grid";
 }
